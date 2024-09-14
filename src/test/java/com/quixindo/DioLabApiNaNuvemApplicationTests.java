f391372aecf52cd01ec983e88c774c3ccc5bf3b4
QuixindoDev <eufrasio.dev@gmail.com>
2024-09-14 01:56:38 +0100

    First commit

---
+++ src/test/java/com/quixindo/DioLabApiNaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quixindo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioLabApiNaNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
